980ea83db792dd07e014a8272cef11c18a201a48
Nick Konkov <kharkivnick@gmail.com>
2025-10-17 23:53:36 +0300

    Fixing models

--- taxi/models.py
+++ taxi/models.py
@@ -14,9 +14,13 @@ class Car(models.Model):
     model = models.CharField(max_length=63)
     manufacturer = models.ForeignKey(
         Manufacturer,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="cars",
     )
-    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    drivers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="drivers"
+    )
 
     def __str__(self):
         return f"{self.manufacturer} ({self.model})"
